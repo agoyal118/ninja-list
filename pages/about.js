18efc121557f25e298d014cafeb6c1ed630a72f0
kyrie irving <kyrie@kyries-MacBook-Pro.local>
2022-02-08 06:01:28 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>This website was created by Graham and Arpit to help those in need.</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
